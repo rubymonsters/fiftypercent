e48577d5b501b46b3e80bfc3f6e21e25029c5c4a
tyranja <tyranja@cassiopeia.uberspace.de>
2014-09-05 13:48:46 +0200

    add create and new method in controller and the related views

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -6,4 +6,39 @@ class EventsController < ApplicationController
   def show
     @event = Event.find(params[:id])
   end
+
+  def new
+    @event = Event.new
+  end
+
+  def edit
+    #@event = Event.find(params[:id])
+  end
+
+  def create
+    @event = Event.new(event_params)
+
+    respond_to do |format|
+      if @event.save
+        format.html { redirect_to @event, notice: 'Event was successfully created.' }
+        format.json { render :show, status: :created, location: @event }
+      else
+        format.html { render :new }
+        format.json { render json: @event.errors, status: :unprocessable_entity }
+      end
+    end
+
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_event
+      @event = Event.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def event_params
+      params.require(:event).permit(:title, :subtitle, :date, :city, :country, :description, :main_url, :speaker_list_url, :contact_url, :woman, :total, :reporter, :reporter_url )
+    end
+
 end
