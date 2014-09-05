9d266489997f6c774ec664442e5a44e0e72ad861
tyranja <tyranja@cassiopeia.uberspace.de>
2014-09-05 14:55:48 +0200

    add validations

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,6 @@
 class Event < ActiveRecord::Base
+  validates :title, :date, :city, :country, :main_url, :total, :woman, :reporter, presence: true
+  validates :total, :woman, numericality: true
 
   def male
     100 - (woman.to_f/total.to_f*100).to_i
