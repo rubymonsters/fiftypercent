4d323c00e4ee27f437061fa2b02162b98e347476
ur5 <urs@so36.net>
2014-10-30 15:04:40 +0100

    make header work better on small screens

--- config/deploy.rb
+++ config/deploy.rb
@@ -32,7 +32,7 @@ set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public
 set :default_env, { path: "/opt/ruby/bin:$PATH" }
 
 # Default value for keep_releases is 5
-# set :keep_releases, 5
+set :keep_releases, 5
 
 namespace :deploy do
 
