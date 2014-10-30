4a2d356eed87e9fd2dcdbe769b1ecc4f86234584
ur5 <urs@so36.net>
2014-10-30 17:49:29 +0100

    adding some basic tests for country code and name

--- test/models/event_test.rb
+++ test/models/event_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class EventTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  # only works it locale == :de
+  test "events know their country name" do
+    assert_equal events(:car_show).country_name, 'Deutschland'
+  end
+  
+  test "a new event has the right name" do
+    assert_equal Event.new(country_code: 'FR').country_name, 'Frankreich'
+  end
 end
