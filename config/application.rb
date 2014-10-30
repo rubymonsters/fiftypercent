8dd61dc262798d0460028625cb2ae6e3f06d58a7
tyranja <tyranja@cassiopeia.uberspace.de>
2014-10-30 14:12:52 +0100

    add fonts

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,8 @@ module Fiftypercent
     config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :de
 
+    config.assets.paths << Rails.root.join("app", "assets", "fonts")
+
     ActsAsTaggableOn.remove_unused_tags = true
   end
 end
