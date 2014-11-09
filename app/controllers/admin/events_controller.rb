15a0858be6e50ecd6378480c10f07aade31f7d31
ur5 <urs@so36.net>
2014-11-09 19:49:03 +0100

    more CRUD for admins on events

--- app/controllers/admin/events_controller.rb
+++ app/controllers/admin/events_controller.rb
@@ -1,31 +1,55 @@
 class Admin::EventsController < Admin::BaseController
 
+  before_action :set_event, only: [:show, :edit, :update, :destroy, :publish, :unpublish]
+
   def index
     @unpublished_events = Event.unpublished.order(:created_at)
 
     if params[:tag].present?
-      @events = Event.order(:created_at).tagged_with(params[:tag]).page(params[:page])
+      @events = Event.order(created_at: :desc).tagged_with(params[:tag]).page(params[:page]).per(40)
     elsif params[:q].present?
-      @events = Event.order(:created_at).search(params[:q]).page(params[:page])
+      @events = Event.order(created_at: :desc).search(params[:q]).page(params[:page]).per(40)
     else
-      @events = Event.order(:created_at).page(params[:page])
+      @events = Event.order(created_at: :desc).page(params[:page]).per(40)
     end
   end
 
   def show
-    @event = Event.find(params[:id])
+  end
+
+  def edit
+  end
+
+
+  def update
+    if @event.update(event_params)
+      redirect_to admin_event_path(@event)
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @event.destroy
+    redirect_to admin_events_path
   end
 
   def publish
-    @event = Event.find(params[:id])
     @event.update_attributes(published_at: Time.now)
     redirect_to admin_events_path
   end
 
   def unpublish
-    @event = Event.find(params[:id])
     @event.update_attributes(published_at: nil)
     redirect_to admin_events_path
   end
 
+  private
+    def event_params
+      params.require(:event).permit!
+    end
+    
+    def set_event
+      @event = Event.find(params[:id])
+    end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,12 +5,13 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(user_params)
-    if @user.save
-      redirect_to root_url, notice: "Signed up!"
-    else
-      render "new"
-    end
+    # TODO: no signup for users, only admins can create users
+    # @user = User.new(user_params)
+    # if @user.save
+    #   redirect_to root_url, notice: "Signed up!"
+    # else
+    #   render "new"
+    # end
   end
 
  private
