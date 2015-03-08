4b70a8bc5502f027befb930cbf8c509acf6d2c42
ur5 <urs@so36.net>
2015-03-08 22:51:59 +0100

    auto-link contact data if twitter-handle or URL

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -4,4 +4,19 @@ module EventsHelper
   def is_single_view
     action_name == 'show'
   end
+
+  def linked_contact_info(contact_info)
+
+    case contact_info
+    when /^@/
+      link_to(contact_info, "https://twitter.com/#{contact_info.gsub(/@/,'')}")
+    when /^http/
+      link_to(contact_info, contact_info)
+    else
+      contact_info
+    end
+
+  end
+
 end
+
