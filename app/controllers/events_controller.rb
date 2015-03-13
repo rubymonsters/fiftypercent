12bc2fac1c47b5a7733808384e2f5db23f387ea9
ur5 <urs@so36.net>
2015-03-13 17:08:51 +0100

    removing motto field

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -58,7 +58,6 @@ class EventsController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def event_params
       params.require(:event).permit(:title,
-                                    :subtitle,
                                     :date,
                                     :city,
                                     :country_code,
