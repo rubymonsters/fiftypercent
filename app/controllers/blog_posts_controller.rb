7bcf8112c3da0ac92bdfc3bf255a07dbd1b0ff5d
ur5 <urs@so36.net>
2015-07-13 22:29:41 +0200

    add export.json path for export of blog-posts to json

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -3,10 +3,23 @@ class BlogPostsController < ApplicationController
   layout 'blog'
 
   def index
-    @posts = Page.blog_posts.order('created_at DESC').all
+    @posts = Page.blog_posts.order('created_at DESC').limit(12).all
   end
 
   def show
     @post = Page.find(params[:id])
   end
+
+  def export
+    respond_to do |format|
+      @posts = Page.blog_posts.order('created_at DESC').limit(12).all
+      @posts_to_export = @posts.map{|post| {title: post.title,
+                                             body: post.body,
+                                              url: blog_post_url(post),
+                                       created_at: post.created_at
+                                            }}
+      format.json { render json: @posts_to_export }
+    end
+  end
+
 end
