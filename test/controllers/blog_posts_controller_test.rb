4448070f4ff60deaf5d5e63a18e0928650bd6511
tyranja <tyranja@cassiopeia.uberspace.de>
2015-12-07 13:04:49 +0100

    Fix texts

--- test/controllers/blog_posts_controller_test.rb
+++ test/controllers/blog_posts_controller_test.rb
@@ -1,8 +1,21 @@
 require 'test_helper'
 
 class BlogPostsControllerTest < ActionController::TestCase
-  # test "should get index" do
-  #   get :index
-  #   assert_response :success
-  # end
+  test "should get show" do
+    blog_post = Page.new(slug: 'a_slug', page_type: 'blog_post', body: 'abc[---]def')
+    blog_post.save!
+
+    get :show, id: blog_post.id
+    assert_response :success
+    assert_not_nil assigns(:post)
+    assert_template :show
+  end
+
+  test "show action loads the right event" do
+    blog_post = Page.new(slug: 'a_slug', page_type: 'blog_post', body: 'abc[---]def')
+    blog_post.save!
+
+    get :show, id: blog_post.id
+    assert_equal assigns(:post), blog_post
+  end
 end
