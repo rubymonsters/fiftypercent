d0da0cfafcbf20fa41be779f971454d41cfdbf87
Tyranja <tyranja@cassiopeia.uberspace.de>
2018-09-20 21:55:56 +0200

    Make test pass

--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -2,7 +2,8 @@ require 'test_helper'
 
 class CommentTest < ActiveSupport::TestCase
   test 'Ensures title, author, body' do
-    comment = Comment.new
+    event = Event.new
+    comment = event.comments.new
     assert_not comment.valid?
     assert_equal 3, comment.errors.count
   end
