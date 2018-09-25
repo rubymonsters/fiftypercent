597217a19eede1b3009eb46a8d74129c503eacac
Tyranja <tyranja@cassiopeia.uberspace.de>
2018-09-25 22:07:20 +0200

    Finish with adding  bootstrap version 4 to comment and event in admin area

--- app/helpers/admin/events_helper.rb
+++ app/helpers/admin/events_helper.rb
@@ -11,7 +11,7 @@ module Admin::EventsHelper
     icon = case event.mod_state
     when 'ok'     then 'thumbs-up'
     when 'hidden' then 'thumbs-down'
-    else 'question-sign'
+    else 'question-circle'
     end
     "<span class=\"fa fa-#{icon}\"></span>&nbsp; mod.-state: <strong>#{event.mod_state}</strong>".html_safe
   end
