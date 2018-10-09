918a92c9b2b5768a6cdfb27614c738464fbf20d6
Tyranja <tyranja@cassiopeia.uberspace.de>
2018-10-09 15:00:38 +0200

    Make sure the error message is shown

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -18,7 +18,7 @@ module ApplicationHelper
 
   def flash_messages(opts = {})
     flash.each do |msg_type, message|
-      concat(content_tag(:div, class: "alert #{bootstrap_class_for(msg_type.to_sym)} fade in") do
+      concat(content_tag(:div, class: "alert #{bootstrap_class_for(msg_type.to_sym)} fade show") do
         concat content_tag(:button, 'x', class: "close", data: { dismiss: 'alert' })
         concat raw(message)
       end)
