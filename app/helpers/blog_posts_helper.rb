fc01e66aacbb8a4f2492aee9c34c737258a47360
zaziemo <maren.heltsche@gmail.com>
2019-08-06 15:11:32 +0200

    Add OG meta tags for blog posts
    to make individual titles, links and pictures visible while sharing on facebook

--- app/helpers/blog_posts_helper.rb
+++ app/helpers/blog_posts_helper.rb
@@ -1,2 +1,11 @@
 module BlogPostsHelper
+  def extract_first_image_path(post)
+    caption, image_path = post.body.match(/\[([^\[\]]+)\]\(([^)]+)/).try(:captures)
+
+    if image_path.present?
+      URI.join(request.base_url, image_path)
+    else
+      URI.join(request.base_url, 'marie-curie-speakerinnen.jpg')
+    end
+  end
 end
